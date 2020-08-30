728210c5d8a7c8fcd1ffe6d9ee815f2c9861d9bf
Ashifis <ashifdesignlab@gmail.com>
2020-08-30 11:39:42 +0530

    Added some new files and links  1

--- Corona-Virus-Report/src/main/java/com/coronavirus/securityLOL/SecurityConfiguration.java
+++ Corona-Virus-Report/src/main/java/com/coronavirus/securityLOL/SecurityConfiguration.java
@@ -33,13 +33,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	}
 
 	
-	
+	/*
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {	
 		 http.authorizeRequests()
 	      .anyRequest().authenticated();
 	}
-	
+	*/
 	
 
 
