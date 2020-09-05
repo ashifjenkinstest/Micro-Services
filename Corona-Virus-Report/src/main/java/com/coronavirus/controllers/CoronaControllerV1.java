32f4316e3da3a2747961d1221e22af59330b7047
Ashifis <ashifdesignlab@gmail.com>
2020-09-05 13:59:29 +0530

    Added add-country api

--- Corona-Virus-Report/src/main/java/com/coronavirus/controllers/CoronaControllerV1.java
+++ Corona-Virus-Report/src/main/java/com/coronavirus/controllers/CoronaControllerV1.java
@@ -9,7 +9,11 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.coronavirus.dummy.Ponto;
 import com.coronavirus.models.CoronaCountryDataStat;
@@ -23,7 +27,7 @@ public class CoronaControllerV1 {
 	@Autowired
 	CoronaVirusDataServiceImpl coronaVirusDataServiceImpl;
 
-	@RequestMapping("/")
+	@RequestMapping(value = "/")
 	public String home(Model model) {
 		coronaVirusDataServiceImpl.getDataFromGitURL();
 		model.addAttribute("fsmessage", "Welcome to the Home page");
@@ -114,5 +118,28 @@ public class CoronaControllerV1 {
 		return "country";
 
 	}
+	
+	@RequestMapping(value = "/add-country", method = RequestMethod.GET)
+	//@ResponseBody
+	public String addCountry(@PathParam(value = "country") String country,
+							 @PathParam(value = "state") String state,
+							 @PathParam(value = "totalCases") String totalCases,
+							 @PathParam(value = "latestCases") String latestCases,
+							 Model model) {
+		String messageStr = null;
+		
+		System.out.println("add-country called");
+		System.out.println(state.length());
+		if (state.length()>1) {
+			 messageStr = country + ", " + state + "\nTotal Cases :" +
+					 		totalCases + " and Latest Cases :" + latestCases + "\nAdded in the Database...";
+		}else {
+		 messageStr = country +  "\nTotal Cases :" + totalCases + " and Latest Cases :" + latestCases + "\nAdded in the Database...";
+		}
+		model.addAttribute("fsmessage", messageStr);
+		return "err";
+		
+	}
+	
 
 }
