ff8848abcc128cc46c4e8edd6c8b0562f189dd9c
Ashifis <ashifdesignlab@gmail.com>
2021-05-12 00:17:29 +0530

    Test some random git issue

--- ipl-dashboard/src/main/java/com/ashifs/model/Team.java
+++ ipl-dashboard/src/main/java/com/ashifs/model/Team.java
@@ -7,7 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
-
+//Test
 @Entity(name = "TEAM")
 public class Team {
 	
