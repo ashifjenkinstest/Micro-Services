076a5ba66adaf9121b7029dede9b134888325b29
Ashifis <ashifdesignlab@gmail.com>
2021-06-24 22:23:27 +0530

    Fix router config

--- cloud-gateway_1/src/main/java/com/ashifs/cloudgateway_1/RouterConfig.java
+++ cloud-gateway_1/src/main/java/com/ashifs/cloudgateway_1/RouterConfig.java
@@ -8,16 +8,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RouterConfig {
 
-    @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-        return builder.routes().route(r -> r.path("/departments/**")
-                .filters(f -> f.hystrix(
-                        h -> h.setName("DEPARTMENT-SERVICE").setFallbackUri("forward:/departmentServiceFallBack")))
-                .uri("lb://DEPARTMENT-SERVICE-EUREKACLIENT").id("DEPARTMENT-SERVICE-EUREKACLIENT")).build()
-                .route(r -> r.path("/users/**").filters(
-                        f -> f.hystrix(h -> h.setName("USER-SERVICE").setFallbackUri("forward:/userServiceFallBack")))
-                        .uri("lb://USER-SERVICE-EUREKACLIENT").id("USER-SERVICE-EUREKACLIENT"))
-                .build();
-    }
+        @Bean
+        public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+                return builder.routes().route(r -> r.path("/departments/**")
+                                .filters(f -> f.hystrix(h -> h.setName("DEPARTMENT-SERVICE")
+                                                .setFallbackUri("forward:/departmentServiceFallBack")))
+                                .uri("lb://DEPARTMENT-SERVICE-EUREKACLIENT").id("DEPARTMENT-SERVICE-EUREKACLIENT"))
+                                .route(r -> r.path("/users/**")
+                                                .filters(f -> f.hystrix(h -> h.setName("USER-SERVICE")
+                                                                .setFallbackUri("forward:/userServiceFallBack")))
+                                                .uri("lb://USER-SERVICE-EUREKACLIENT").id("USER-SERVICE-EUREKACLIENT"))
+                                .build();
+        }
 
 }
